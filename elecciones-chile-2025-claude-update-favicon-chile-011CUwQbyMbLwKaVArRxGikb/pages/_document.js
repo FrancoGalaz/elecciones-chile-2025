3c3dc76b7378ad244e27401e85e48affc89dfe87
Franco <francog.dev@gmail.com>
2025-11-08 22:32:38 -0300

    Head

---
+++ elecciones-chile-2025-claude-update-favicon-chile-011CUwQbyMbLwKaVArRxGikb/pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2991938455307697"
+     crossorigin="anonymous"></script>
+        <link rel="icon" href="/favicon-chile.ico" />
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
